a42093b8b2fbce652df473155a2d9e7b00a38912
linmou <18302010075@fudan.edu.cn>
2021-04-06 00:05:44 +0800

    feat: 增加接口

--- src/main/java/com/bank/bank/controller/FinanceProductController.java
+++ src/main/java/com/bank/bank/controller/FinanceProductController.java
@@ -1,14 +1,20 @@
 package com.bank.bank.controller;
 
 
+import com.bank.bank.entity.FinanceProduct;
+import com.bank.bank.service.FinanceProductService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * <p>
- *  前端控制器
- * </p>
+ *
+ *  理财产品
+ *
  *
  * @author yuwen
  * @since 2021-03-28
@@ -17,4 +23,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/bank/finance-product")
 public class FinanceProductController {
 
+    @Autowired
+    private FinanceProductService financeProductService;
+
+    /**
+     * 查看理财产品列表
+     * @return
+     */
+    @GetMapping
+    @ApiOperation(value = "查看理财产品列表")
+    public Page<FinanceProduct> getProducts(){
+        return financeProductService.getProducts();
+    }
 }
--- src/main/java/com/bank/bank/controller/LoanRecordController.java
+++ src/main/java/com/bank/bank/controller/LoanRecordController.java
@@ -1,13 +1,24 @@
 package com.bank.bank.controller;
 
 
+import com.bank.bank.entity.Account;
+import com.bank.bank.entity.LoanRecord;
+import com.bank.bank.service.AccountService;
+import com.bank.bank.service.LoanRecordService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ *  贷款记录
  * </p>
  *
  * @author yuwen
@@ -17,4 +28,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/bank/loan-record")
 public class LoanRecordController {
 
+    @Autowired
+    private LoanRecordService loanRecordService;
+
+    /**
+     * 按照用户id查询对应的贷款记录
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/{id}")
+    @ApiOperation(value = "按照用户id查询对应的贷款记录")
+    public Page<LoanRecord> findByIDN(@PathVariable(value = "id")@ApiParam(value = "用户id") Long id){
+        return loanRecordService.getByUserId(id);
+    }
 }
