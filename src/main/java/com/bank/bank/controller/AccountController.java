bef54bb140049013b0e70439f9c7f7eb2fb7d9d0
linmou <18302010075@fudan.edu.cn>
2021-04-06 00:06:15 +0800

    feat: 增加account接口

--- src/main/java/com/bank/bank/controller/AccountController.java
+++ src/main/java/com/bank/bank/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.bank.bank.controller;
 
 
+import com.bank.bank.dto.AccountDto;
 import com.bank.bank.entity.Account;
 import com.bank.bank.service.AccountService;
 import io.swagger.annotations.Api;
@@ -9,18 +10,14 @@ import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.ws.Response;
 
 /**
  * <p>
- *  前端控制器
+ *  账户接口
  * </p>
  *
  * @author yuwen
@@ -33,9 +30,24 @@ public class AccountController {
     @Autowired
     private AccountService accountService;
 
+    /**
+     * 按照身份证查询账户
+     * @param idn
+     * @return
+     */
     @GetMapping(value = "/{idn}")
     @ApiOperation(value = "根据身份证查询账户")
     public ResponseEntity<Account> findByIDN(@PathVariable(value = "idn")@ApiParam(value = "身份证号") Long idn){
         return ResponseEntity.ok(accountService.getByIDN(idn));
     }
+
+    /**
+     * 新增账户
+     */
+    @PostMapping(value = "create")
+    @ApiOperation(value = "新增账户")
+    public ResponseEntity create(@RequestBody AccountDto accountDto){
+        return ResponseEntity.ok("新增账户成功");
+    }
+
 }
---
+++ src/main/java/com/bank/bank/dto/AccountDto.java
@@ -0,0 +1,49 @@
+package com.bank.bank.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+import lombok.Data;
+
+@Data
+@ApiModel(value = "账户")
+public class AccountDto {
+
+    /**
+     * 身份证号
+     */
+    @ApiModelProperty(value = "身份证号")
+    private Long idNumber;
+
+    /**
+     * 余额
+     */
+    @ApiModelProperty(value = "账户余额")
+    private Double balance;
+
+    /**
+     * 信用等级
+     */
+    @ApiModelProperty(value = "信用等级")
+    private Integer creditRate;
+
+    /**
+     * 贷款金额
+     */
+    @ApiModelProperty(value = "贷款金额")
+    private Double loanAmount;
+
+    /**
+     * 姓名
+     */
+    @ApiModelProperty(value = "姓名")
+    private String name;
+
+    /**
+     * 年龄
+     */
+    @ApiModelProperty(value = "年龄")
+    private int age;
+
+
+}
