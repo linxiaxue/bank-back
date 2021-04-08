071f2b71201ff7f881cce70478cfa43974670819
linmou <18302010075@fudan.edu.cn>
2021-04-09 00:47:15 +0800

    feat：时间接口

---
+++ src/main/java/com/bank/bank/controller/baseController.java
@@ -0,0 +1,38 @@
+package com.bank.bank.controller;
+
+import com.bank.bank.service.AccountService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * 基础接口
+ */
+@RestController
+@RequestMapping("/bank")
+@Api(value = "基础")
+public class baseController {
+    private AccountService accountService;
+
+    /**
+     * 获取当前时间
+     * @return
+     */
+    @GetMapping("/time")
+    @ApiOperation(value = "获取当前时间")
+    public ResponseEntity<Date> getNowTime(){
+        return ResponseEntity.ok(accountService.getNowTime());
+    }
+
+    @GetMapping("/addTime")
+    @ApiOperation(value = "增加一天")
+    public ResponseEntity<String> addTime(){
+        return ResponseEntity.ok(accountService.addTime());
+    }
+
+}
